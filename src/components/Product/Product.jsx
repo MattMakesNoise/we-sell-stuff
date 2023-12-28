ce283b3a1a98220d9de21215eb3af0d8c21b326a
MattMakesNoise <matthewjones81@gmail.com>
2023-12-28 20:53:51 +0000

    style: adds global styles, mixins and variables and gives some more styling to the product page.

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import './Product.scss';
 
 const Product = ({ data }) => {
     const { id } = useParams();
     const product = data.find(item => item.ID === id);
+    // gets. the product.INFO and strips the returned string of quotation marks.
+    const info = product.INFO.slice(1, product.INFO.length - 1);
 
     return (
-        <div>
-            <h1>{product.TITLE}</h1>
-            <img src={product['IMG-HREF']} alt={product.TITLE} />
-            <p>{product.DETAILS}</p>
-            <p>Price: {product.PRICE}</p>
+        <div className='product'>
+            <div className="image-particulars-wrap">
+                <div className="image-wrap">
+                    <img src={product['IMG-HREF']} alt={product.TITLE} />
+                </div>
+                
+                <div className='product-particulars'>
+                    <h1>{product.TITLE}</h1>
+                    <h2>{info}</h2>
+                    <p>Price: {product.PRICE}</p>
+                    <p>Category: {product.CATEGORY}</p>
+                </div>
+            </div>
+            
+            <div className="product-extras">
+                EXTRA, EXTRA
+            </div>
         </div>
     );
 };
